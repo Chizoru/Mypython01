0e18ca5328b8a6be8bb54b759730042f699b6cf7
s6852d10031-boop <S6852D10031@sau.ac.th>
2025-08-22 12:02:31 +0700

    V5 Test9-10 Complete

---
+++ test10.py
@@ -0,0 +1,47 @@
+# สร้างโปรแกรมรับข้อมูลจำนวนเต็ม 5 จำนวนจากผู้ใช้
+# และคำนวณหาผลรวม และค่าเฉลี่ยของข้อมูลที่รับเข้ามา
+# เป็นเท่าใด แล้วแสดงผลทางหน้าจอ
+# สูตร  
+# ผลรวม = เลขตัวที่1 + เลขตัวที่2 + เลขตัวที่3 + เลขตัวที่4 + เลขตัวที่5
+# ค่าเฉลี่ย = ผลรวมของเลข 5 จำนวน / 5
+
+# ============================
+# Program  Average  5  Number
+# ============================
+# Enter number 1 : <input>
+# Enter number 2 : <input>
+# Enter number 3 : <input>
+# Enter number 4 : <input>
+# Enter number 5 : <input>
+# ============================
+# Sum of 5 number is : <output>
+# Average of 5 number is : <output>
+# ============================
+# Sum of 5 number is : <output>
+# Average of 5 number is : <output>
+# ============================
+# Sum of 5 number is : <output>
+# Average of 5 number is : <output>
+# ============================
+# Sum of 5 number is : <output>
+# Average of 5 number is : <output>
+# ============================
+print('+++++++++++++++++++++++++')
+print('Program  Average  5  Number')
+print('+++++++++++++++++++++++++')
+print('Enter number 1 : ', end='')
+num1 = int(input())
+print('Enter number 2 : ', end='')
+num2 = int(input())
+print('Enter number 3 : ', end='')
+num3 = int(input())
+print('Enter number 4 : ', end='')
+num4 = int(input())
+print('Enter number 5 : ', end='')
+num5 = int(input())
+print('+++++++++++++++++++++++++')
+print('Sum of 5 number is : ', end='')
+print(num1 + num2 + num3 + num4 + num5)
+print('Average of 5 number is : ', end='')
+print((num1 + num2 + num3 + num4 + num5) / 5)
+print('+++++++++++++++++++++++++')
